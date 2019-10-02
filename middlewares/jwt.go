72046d7545f05bb2cc81a6f2d2cf65eb250869cf
Go Go Go <xiangrui@aiursoft.com>
2019-10-03 07:56:08 +0800

    jwt

--- middlewares/jwt.go
+++ middlewares/jwt.go
@@ -9,6 +9,12 @@ type JwtMiddleware struct {
 	JwtModule *jwtm.RedisJWT
 }
 
+func NewJwtMiddleware(jwt_module *jwtm.RedisJWT) *JwtMiddleware {
+	return &JwtMiddleware{
+		JwtModule: jwt_module,
+	}
+}
+
 func (middleware *JwtMiddleware) New() gin.HandlerFunc {
 	return func(context *gin.Context) {
 		// 获取头信息
